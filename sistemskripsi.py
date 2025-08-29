f9d352a64598fc2ac8a4056c96ada5a8f3f02a81
NabilaAtiraQurratulAini <107897951+NabilaAtiraQurratulAini@users.noreply.github.com>
2025-08-29 22:46:30 +0700

    Update sistemskripsi.py

--- sistemskripsi.py
+++ sistemskripsi.py
@@ -130,9 +130,10 @@ if st.button("🔮 Prediksi"):
         label_text = "Churn" if label == 1 else "Tidak Churn"
 
         st.success(f"🔣 Hasil Prediksi: **{label_text}**")
-        st.metric(label="🌟 Probabilitas Churn:", value={prob:.2%}")
+        st.metric(label="🌟 Probabilitas Churn:", value=f"{prob:.2%}")
     except Exception as e:
         st.error(f"Terjadi kesalahan saat memuat model: {e}")
+
 
 
 
